13485a3381667e3cdf9d3a50c1462e32d64affe7
lemonjai <leocheung_1986@hotmail.com>
2016-03-26 00:12:32 -0400

    categories dynamic in show page and link to books now

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,9 +25,13 @@ class CategoriesController < ApplicationController
   end
 
   def index
+    @categories = Category.all
   end
 
   def show
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @books = @category.books
   end
 
   private
